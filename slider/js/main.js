cd197710e5a9aa3039d2ad28b825d4be4a17a6f2
npc-sapsan <45822464+npc-sapsan@users.noreply.github.com>
2018-12-12 16:48:23 +0300

    Add files via upload

---
+++ slider/js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    const crs = $("#carousel");
+    crs.Cloud9Carousel({
+        yOrigin: 42,
+        yRadius: 40,
+        itemClass: "card",
+        bringToFront: true,
+        onLoaded: function () {
+            crs.css('visibility', 'visible')
+            crs.css('display', 'none')
+            crs.fadeIn(1500)
+        },
+        speed:2,
+        mirror: {
+            gap: 12,     /* 12 pixel gap between item and reflection */
+            height: 0.2, /* 20% of item height */
+            opacity: 0.4 /* 40% opacity at the top */
+        }
+    });
+});
+
+function fillname() {
+    const info = document.querySelector('#info');
+    info.textContent = this.dataset.name;
+}
+const cards = document.querySelectorAll(".card");
+cards.forEach(card=>card.addEventListener('click', fillname));
+
+antsiforov4
