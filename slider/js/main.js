d16d656465c0a1f504ddbf58b68fe560c646019b
npc-sapsan <45822464+npc-sapsan@users.noreply.github.com>
2018-12-14 15:03:51 +0300

    Add files via upload

--- slider/js/main.js
+++ slider/js/main.js
@@ -1,6 +1,59 @@
 $(function(){
+
     const crs = $("#carousel");
+
+    const data = [
+        {
+            'name':"«НПЦ «Сапсан»",
+            'logo':"npc-sapsan_sm.jpg"
+        },
+        {
+            'name':"АО «Автоматика»",
+            'logo':"avtomatika_sm.jpg"
+        },
+        {
+            'name':"АО «Брянский электромеханический завод»",
+            'logo':"bryansk_sm.jpg"
+        },
+        {
+            'name':"ПАО «Брянское специальное конструкторское бюро»",
+            'logo':"bskb_sm.jpg"
+        },
+        {
+            'name':"АО «Всероссийский научно-исследовательский институт «Градиент»",
+            'logo':"gradient_sm.jpg"
+        },
+        {
+            'name':"АО «Научно-производственное объединение «Квант»",
+            'logo':"kvant_sm.jpg"
+        },
+        {
+            'name':"АО «Научно-производственное объединение «Радиоэлектроника» имени В.И.Шимко»",
+            'logo':"shimko_sm.jpg"
+        },
+        {
+            'name':"АО «Радий»",
+            'logo':"radii_sm.jpg"
+        },
+        {
+            'name':"АО «Научно-исследовательский институт авиационного оборудования»",
+            'logo':"aviapribor_sm.jpg"
+        }
+    ];
+
+    data.forEach(item=>{
+        let el = `
+        <div class="card" data-name="${item.name}">
+            <img src="img/${item.logo}" alt="${item.name}"/>
+            <!--p><a href="#">${item.name}</a></p-->
+        </div>
+        `;
+        crs.append(el);
+    })
+
     crs.Cloud9Carousel({
+        buttonLeft: $(".slider-control-left"),
+        buttonRight: $(".slider-control-right"),
         yOrigin: 42,
         yRadius: 40,
         itemClass: "card",
@@ -10,19 +63,15 @@ $(function(){
             crs.css('display', 'none')
             crs.fadeIn(1500)
         },
-        speed:2,
-        frontItemClass: 'carousel-big',
-        mirror: {
-            gap: 12,     /* 12 pixel gap between item and reflection */
-            height: 0.2, /* 20% of item height */
-            opacity: 0.4 /* 40% opacity at the top */
-        }
+        speed:1,
+        frontItemClass: 'carousel-big'
     });
-});
 
-function fillname() {
-    const info = document.querySelector('#info');
-    info.textContent = this.dataset.name;
-}
-const cards = document.querySelectorAll(".card");
-cards.forEach(card=>card.addEventListener('click', fillname));
+    function fillname() {
+        const info = document.querySelector('#info');
+        info.textContent = this.dataset.name;
+    }
+    const cards = document.querySelectorAll(".card");
+    cards.forEach(card=>card.addEventListener('click', fillname));
+    document.querySelector('#info').textContent = data[0].name;
+});
