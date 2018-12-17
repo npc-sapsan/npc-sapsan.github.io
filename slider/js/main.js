e0e661f0670e38d73873d88eaa9170fc516fa4b4
npc-sapsan <45822464+npc-sapsan@users.noreply.github.com>
2018-12-17 12:41:33 +0300

    Add files via upload

--- slider/js/main.js
+++ slider/js/main.js
@@ -4,46 +4,65 @@ $(function(){
 
     const data = [
         {
+            id: 0,
             'name':"«НПЦ «Сапсан»",
             'logo':"npc-sapsan_sm.jpg"
         },
         {
+            id: 1,
             'name':"АО «Автоматика»",
             'logo':"avtomatika_sm.jpg"
         },
         {
+            id: 2,
             'name':"АО «Брянский электромеханический завод»",
             'logo':"bryansk_sm.jpg"
         },
         {
+            id: 3,
             'name':"ПАО «Брянское специальное конструкторское бюро»",
             'logo':"bskb_sm.jpg"
         },
         {
+            id: 4,
             'name':"АО «Всероссийский научно-исследовательский институт «Градиент»",
             'logo':"gradient_sm.jpg"
         },
         {
+            id: 5,
             'name':"АО «Научно-производственное объединение «Квант»",
             'logo':"kvant_sm.jpg"
         },
         {
+            id: 6,
             'name':"АО «Научно-производственное объединение «Радиоэлектроника» имени В.И.Шимко»",
             'logo':"shimko_sm.jpg"
         },
         {
+            id: 7,
             'name':"АО «Радий»",
             'logo':"radii_sm.jpg"
         },
         {
+            id: 8,
             'name':"АО «Научно-исследовательский институт авиационного оборудования»",
             'logo':"aviapribor_sm.jpg"
         }
     ];
 
+    function getDataElement(id){
+        let found = null;
+        data.forEach(el=> {
+            if(el['id'] == id) {
+                found = el;
+            }
+        });
+        return found;
+    };
+
     data.forEach(item=>{
         let el = `
-        <div class="card" data-name="${item.name}">
+        <div class="card" id="${item.id}" data-name="${item.name}">
             <img src="img/${item.logo}" alt="${item.name}"/>
             <!--p><a href="#">${item.name}</a></p-->
         </div>
@@ -59,13 +78,23 @@ $(function(){
         itemClass: "card",
         bringToFront: true,
         onLoaded: function () {
-            crs.css('visibility', 'visible')
-            crs.css('display', 'none')
-            crs.fadeIn(1500)
+            crs.css('visibility', 'visible');
+            crs.css('display', 'none');
+            crs.fadeIn(1500);
+        },
+        onAnimationFinished: function(){
+            let idx = crs.data("carousel").nearestIndex();
+            let el = getDataElement(idx);
+            document.querySelector('#info').textContent = el.name;
         },
         speed:1,
         frontItemClass: 'carousel-big'
     });
+
+    // buttons
+    document.querySelector('#print').addEventListener('click', function () {
+        print();
+    })
 
     function fillname() {
         const info = document.querySelector('#info');
@@ -82,22 +111,27 @@ $(function(){
         });
     };
 
+    let suggLi = document.querySelectorAll('.suggestions li');
     function displayMatches(){
         const matchArray = findMatches(this.value, data);
         const html = matchArray.map(org=>{
             const regex = new RegExp(this.value, 'gi');
-            const orgName = org.name.replace(regex, `<span class='hl'>${this.value}</span>`);
+            const orgName = org.name.replace(regex, `${this.value}`);
             return `
       <li>
-        <span class="name">${orgName}</span>
+        <span class="name" data-id="${org.id}">${orgName}</span>
       </li>  
     `;
         }).join('');
         suggestions.innerHTML = html;
+        suggLi = document.querySelectorAll('.suggestions li');
+        suggLi.forEach(li=>li.addEventListener('click',function(){
+            let selectedId = li.firstChild.nextSibling.dataset.id;
+            crs.data('carousel').goTo(selectedId);
+        }));
     };
 
     const searchInput = document.querySelector('#search');
     const suggestions = document.querySelector('.suggestions');
     searchInput.addEventListener('keyup', displayMatches);
-
 });
